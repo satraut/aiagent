49796e697fa9cbd235ca8dcfce50c29c58687b73
satraut <satyamraut07@gmail.com>
2025-09-01 17:13:20 +0530

    new commit

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import os
+from dotenv import load_dotenv
+import sys
+from google import genai
+from google.genai import types
+
+
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+from google import genai
+
+client = genai.Client(api_key=api_key)
+if len(sys.argv)<2:
+    sys.exit(1)
+user_prompt= sys.argv[1]
+verbose = "--verbose" in sys.argv[2:]
 def main():
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
     print("Hello from aiagent!")
+    model = "gemini-2.0-flash-001"
+    content = "what is the weather in pune"
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    print(response.text)
+    if verbose:
+        print(f'User prompt: {user_prompt}')
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
